6eddae5a10df014eb27c9492c71270c5cb7c37f0
Courtney Carson <cmacarson@gmail.com>
2020-10-28 14:13:32 -0700

    update, delete tests

--- test/articles-service.spec.js
+++ test/articles-service.spec.js
@@ -42,7 +42,7 @@ describe('Articles service object', function () {
   })
   before(() => db('blogful_articles').truncate())
 
-// clean up after ourselves with aftereach block to remove all data for each test
+  // clean up after ourselves with aftereach block to remove all data for each test
   afterEach(() => db('blogful_articles').truncate())
 
   // before(() => {
@@ -58,10 +58,55 @@ describe('Articles service object', function () {
   //context is a synonym for describe
   // describe(`getAllArticles()`, () => {
   context(`Given 'blogful_articles' has data`, () => {
-    before(() => {
+    beforeEach(() => {
       return db
         .into('blogful_articles')
         .insert(testArticles)
+    })
+    // Update an article
+    // it(`updateArticle() updates an article from the 'blogful_articles' table`, () => {
+    //   const idOfArticleToUpdate = 3
+    //   const newArticleData = {
+    //     title: 'updated title',
+    //     content: 'updated content',
+    //     date_published: new Date(),
+    //   }
+    //   return ArticlesService.updateArticle(db, idOfArticleToUpdate, newArticleData)
+    //     .then(() => ArticlesService.getById(db, idOfArticleToUpdate))
+    //     .then(article => {
+    //       expect(article).to.eql({
+    //         id: idOfArticleToUpdate,
+    //         ...newArticleData,
+    //       })
+    //     })
+    // })
+
+    //delete article
+    it(`deleteArticle() removes an article by id from 'blogful_articles' table`, () => {
+      const articleId = 3
+      return ArticlesService.deleteArticle(db, articleId)
+        .then(() => ArticlesService.getAllArticles(db))
+        .then(allArticles => {
+          // copy the test articles array without the "deleted" article
+          const expected = testArticles.filter(article => article.id !== articleId)
+          expect(allArticles).to.eql(expected)
+        })
+    })
+  
+
+    // get a specific article
+    it(`getById() resolves an article by id from 'blogful_articles' table`, () => {
+      const thirdId = 3
+      const thirdTestArticle = testArticles[thirdId - 1]
+      return ArticlesService.getById(db, thirdId)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: thirdId,
+            title: thirdTestArticle.title,
+            content: thirdTestArticle.content,
+            date_published: thirdTestArticle.date_published,
+          })
+        })
     })
     // })
 
@@ -83,6 +128,21 @@ describe('Articles service object', function () {
           expect(actual).to.eql([])
         })
     })
+    it(`insertArticle() inserts a new article and resolves the new article with an 'id'`, () => {
+      const newArticle = {
+        title: 'Test new title',
+        content: 'Test new content',
+        date_published: new Date('2020-01-01T00:00:00.000Z'),
+      }
+      return ArticlesService.insertArticle(db, newArticle)
+        .then(actual => {
+          expect(actual).to.eql({
+            id: 1,
+            title: newArticle.title,
+            content: newArticle.content,
+            date_published: newArticle.date_published,
+          })
+        })
+    })
   })
-
 })
